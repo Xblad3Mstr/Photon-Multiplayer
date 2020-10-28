64ed91e2028a8056d5d548cea9e30f46390496f0
Xblad3Mstr <46698999+Xblad3Mstr@users.noreply.github.com>
2020-10-28 15:40:02 -0600

    Started making title screen

---
+++ Photon_Billiards/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public int startButton;
+    public int exitButton;
+
+    public Scene gameScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+        if (Input.GetMouseButtonDown(exitButton))
+        {
+            Application.Quit();
+        }
+    }
+
+    private void LoadScene(Scene sceneName)
+    {
+        //  if (Input.GetMouseButtonDown(startButton))
+        //{
+        //  SceneManager.LoadScene("GameScene");
+        // }
+        SceneManager.LoadScene(gameScene);
+    }
+}
