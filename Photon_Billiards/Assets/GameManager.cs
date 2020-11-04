40da2d0dd008c216c4a25db253fd704d6174ca08
RasmussenL <46792462+RasmussenL@users.noreply.github.com>
2020-11-03 18:26:51 -0700

    WorkingPowerBar
    hold mb, drag down, release. working on camera control next

--- Photon_Billiards/Assets/GameManager.cs
+++ Photon_Billiards/Assets/GameManager.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody cue;
+    [SerializeField]
+    private Image powerBar;
+    [SerializeField]
+    private float power = 500f;
+    private Vector3 startingMouse;
+    private float powerMultiplier;
 
-    private float power = 200f;
+    private void Start()
+    {
+        powerBar.fillAmount = 0f;
+    }
+
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
-            cue.AddForce(transform.forward * power); 
+            startingMouse = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            //float distance = Vector3.Distance(startingMouse, Input.mousePosition);
+            float distance = (startingMouse - Input.mousePosition).y;
+            powerBar.fillAmount = distance / 200;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            powerMultiplier = powerBar.fillAmount;
+            cue.AddForce(transform.forward * power * powerMultiplier);
+            powerBar.fillAmount = 0f;
         }
     }
 }
