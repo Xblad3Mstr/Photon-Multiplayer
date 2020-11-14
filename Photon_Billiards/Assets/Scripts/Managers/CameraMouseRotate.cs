21e4ba333aea87b5d2e170371da494e82ffd076a
Taylor Renner <britenyspaniel@yahoo.com>
2020-11-13 21:45:59 -0700

    Ball Respawns when hit off table

---
+++ Photon_Billiards/Assets/Scripts/Managers/CameraMouseRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseRotate : MonoBehaviour
+{
+    [HideInInspector]
+    Camera playerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = 5 * Input.GetAxis("Mouse X");
+        float y = 5 * -Input.GetAxis("Mouse Y");
+        Camera.current.transform.Rotate(x, y, 0);
+    }
+}
